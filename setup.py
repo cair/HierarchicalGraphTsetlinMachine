67daff8fabb9baca542fca6057c2a5fbe0b1690e
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-05-27 13:14:40 +0100

    Updates

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import *
 
 setup(
-   name='GraphTsetlinMachine',
+   name='HierarchicalGraphTsetlinMachine',
    version='0.3.4',
    author='Ole-Christoffer Granmo',
    author_email='ole.granmo@uia.no',
-   url='https://github.com/cair/GraphTsetlinMachine/',
+   url='https://github.com/cair/HierarchicalGraphTsetlinMachine/',
    license='MIT',
-   description='Graph Tsetlin Machine',
-   long_description='Graph Tsetlin Machine for Deep Logical Learning and Reasoning',
+   description='HierarchicalGraph Tsetlin Machine',
+   long_description='Hierarchical Graph Tsetlin Machine for Deep Logical Learning and Reasoning',
    keywords ='pattern-recognition cuda machine-learning interpretable-machine-learning rule-based-machine-learning propositional-logic graph tsetlin-machine regression convolution classification multi-layer',
-   packages=['GraphTsetlinMachine']
+   packages=['HierarchicalGraphTsetlinMachine']
 )
